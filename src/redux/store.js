22d3be41e353f91db842fc6e9aa401b9a8f309ad
Ayxan <gnessdo@gmail.com>
2024-04-25 21:09:08 +0400

    build

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import {configureStore, getDefaultMiddleware} from "@reduxjs/toolkit"
-// import cartSlice from "./slices/cart.slice"
-import { cartReducer } from "./slices/cart.slice";
+import cartSlice from "./slices/cart.slice"
 import authSlice from "./slices/Auth.slice";
 import categorySlice from "./slices/category.slice";
 
@@ -8,7 +7,7 @@ import categorySlice from "./slices/category.slice";
 
 const store  = configureStore({
     reducer: {
-        cart: cartReducer,
+        cart: cartSlice,
         auth: authSlice,
         category: categorySlice,
 
