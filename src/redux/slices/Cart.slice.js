53a7f523696cb31a755be91b9b85490a2adad48c
Ayxan <gnessdo@gmail.com>
2024-04-25 23:25:37 +0400

    build

--- src/redux/slices/Cart.slice.js
+++ src/redux/slices/Cart.slice.js
@@ -1,33 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+  import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-    //Burada biz diyoruz ki, getItem ile cartItems güncellendiğinde, değerler sıfırlanmasın. 
-    //Yani cart state'i localStorage'a yazıldığında ve içi doluysa, silinmesin, boşsa boş görünsün 
-    cart: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
-};
+  const initialState = {
+      //Burada biz diyoruz ki, getItem ile cartItems güncellendiğinde, değerler sıfırlanmasın. 
+      //Yani cart state'i localStorage'a yazıldığında ve içi doluysa, silinmesin, boşsa boş görünsün 
+      cart: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
+  };
 
-const cartSlice = createSlice({
-    name: 'cart',
-    initialState,
-    reducers: {
-        addToCart: (state, action) => {
-          const {id, quantity, ...cart} = action.payload;
-          // Eğer ürün zaten sepete eklenmişse, sadece miktarını güncelle
-          const existingProduct = state.cart.find(item => item.id === id);
-          if(existingProduct){
-            existingProduct.quantity += quantity;
-          } else {
-            state.cart.push({id, quantity, ...cart});
+  const cartSlice = createSlice({
+      name: 'cart',
+      initialState,
+      reducers: {
+          addToCart: (state, action) => {
+            const {id, quantity, ...cart} = action.payload;
+            // Eğer ürün zaten sepete eklenmişse, sadece miktarını güncelle
+            const existingProduct = state.cart.find(item => item.id === id);
+            if(existingProduct){
+              existingProduct.quantity += quantity;
+            } else {
+              state.cart.push({id, quantity, ...cart});
+            }
+          },
+          removeFromCart: (state, action) => {
+              state.cart = state.cart.filter(item => item.id !== action.payload);
+          },
+          setCartItems:(state, action) => {
+            state.cart = action.payload; // Yerel depolamadan yüklenen öğeleri ata
           }
-        },
-        removeFromCart: (state, action) => {
-            state.cart = state.cart.filter(item => item.id !== action.payload);
-        },
-        setCartItems:(state, action) => {
-          state.cart = action.payload; // Yerel depolamadan yüklenen öğeleri ata
-        }
-    },
-});
+      },
+  });
 
-export const { addToCart, removeFromCart, setCartItems } = cartSlice.actions;
-export default cartSlice.reducer;
+  export const { addToCart, removeFromCart, setCartItems } = cartSlice.actions;
+  export default cartSlice.reducer;
